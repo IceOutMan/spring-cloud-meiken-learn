7a6315f5ec730b32073a84a22fae2b9d0567cd4d
meiken.gu <aluo_fei@163.com>
2023-02-22 18:48:41 +0800

    init

---
+++ spring-cloud-meiken-zookeeper/spring-cloud-meiken-zookeeper-register/spring-cloud-meiken-zookeeper-register-product/src/main/java/com/meiken/SpringCloudMeikenZookeeperRegisterProductApplication.java
@@ -0,0 +1,13 @@
+package com.meiken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudMeikenZookeeperRegisterProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudMeikenZookeeperRegisterProductApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-meiken-zookeeper/spring-cloud-meiken-zookeeper-register/spring-cloud-meiken-zookeeper-register-product/src/test/java/com/meiken/SpringCloudMeikenZookeeperRegisterProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meiken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudMeikenZookeeperRegisterProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
